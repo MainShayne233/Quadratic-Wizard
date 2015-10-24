503a3a47525b405365ac20825127f7280f0293a6
Main0Shayne <shaynetremblay@hotmail.com>
2015-10-23 22:30:59 -0400

    Initial Commit

---
+++ Quadratic WizardTests/Quadratic_WizardTests.swift
@@ -0,0 +1,36 @@
+//
+//  Quadratic_WizardTests.swift
+//  Quadratic WizardTests
+//
+//  Created by shayne on 10/23/15.
+//  Copyright © 2015 Open Sourcery. All rights reserved.
+//
+
+import XCTest
+@testable import Quadratic_Wizard
+
+class Quadratic_WizardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
