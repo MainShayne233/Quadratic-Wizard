dd3685b7b517af1c40ddd74f75f62bcb9964809e
Main0Shayne <shaynetremblay@hotmail.com>
2015-10-23 23:15:30 -0400

    first init

--- Quadratic Wizard/ViewController.swift
+++ Quadratic Wizard/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var a_jax: UIWebView!
+    @IBOutlet weak var b_jax: UIWebView!
+    @IBOutlet weak var c_jax: UIWebView!
+    @IBOutlet weak var s_jax: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        a_jax.loadRequest(NSURLRequest(URL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("a_html", ofType: "html")!)))
+        b_jax.loadRequest(NSURLRequest(URL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("b_html", ofType: "html")!)))
+        c_jax.loadRequest(NSURLRequest(URL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("c_html", ofType: "html")!)))
+        s_jax.loadRequest(NSURLRequest(URL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("2_s_html", ofType: "html")!)))
     }
 
     override func didReceiveMemoryWarning() {
